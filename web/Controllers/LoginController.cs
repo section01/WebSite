e3e55a5e0b1c03d9ca7c3be26c9a09e22220a5a3
近澤圭佑 <ke-chikazawa@ac-tec.co.jp>
2022-06-23 19:06:04 +0900

    update

---
+++ web/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace web.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Init()
+        {
+            return View("login");
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return View("login");
+        }
+    }
+}
